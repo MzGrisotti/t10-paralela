974cd047b2958ee9b1c4155dac6b3a5c9159c6d8
kadykunde <kadykunde@gmail.com>
2019-07-10 03:17:05 -0300

    Primeira versão cuda

--- main_paralel2.cu
+++ main_paralel2.cu
@@ -12,16 +12,13 @@
 #define PI 3.1415
 
 //Cube_Unity ***Cube;
-Cube_Unity *Cube;
+//Cube_Unity *Cube;
 //Cube_Unity ***Cube_Perspective;
-Cube_Unity *Cube_Perspective;
-int Size;
-int width;
-int OffSet;
+//Cube_Unity *Cube_Perspective;
 
 //funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
 //globais que podem ser setadas pelo metodo keyboard()
-
+/*
 void OffSet_Z(double offset){
 
    for(auto i = 0; i < Size; i++){
@@ -138,18 +135,27 @@ void Rotate_z(double angulo){
    }
 
 }
+*/
 
-void render(){
+void render(Cube_Unity *Cube, Cube_Unity *Cube_Perspective){
+	int Size = 100;
+	int width = 2000;
+	int OffSet = 3000;
    double startAngulo = 1;
    //Rotate_z(angulo);
    double offset = startAngulo;
-	for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+   
+   auto k = blockIdx.x;
+   int i = int(floor(threadIdx.x/10));
+   
+	//for(auto i = 0; i < Size; i++){
+      //for(auto j = 0; j < Size; j++){
+	  for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
             Cube[i + Size * (j + Size * k)].z += offset;
-         }
+         //}
       }
-   }
+   //}
    
    //Rotate_y(angulo);
 	double angulo = startAngulo * PI/180;
@@ -159,11 +165,11 @@ void render(){
                            -sin(angulo), 0, cos(angulo), 0,
                                       0, 0,           0, 1};
 
-   //double new_x, new_y, new_z;
+   double new_x, new_y, new_z;
 
-   for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+   //for(auto i = 0; i < Size; i++){
+      for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
 
             new_x = Cube[i + Size * (j + Size * k)].x * matrix_x[0][0] + Cube[i + Size * (j + Size * k)].y * matrix_x[0][1] + Cube[i + Size * (j + Size * k)].z * matrix_x[0][2];
             new_y = Cube[i + Size * (j + Size * k)].x * matrix_x[1][0] + Cube[i + Size * (j + Size * k)].y * matrix_x[1][1] + Cube[i + Size * (j + Size * k)].z * matrix_x[1][2];
@@ -173,9 +179,9 @@ void render(){
             Cube[i + Size * (j + Size * k)].y = new_y;
             Cube[i + Size * (j + Size * k)].z = new_z;
 
-         }
+         //}
       }
-   }
+   //}
    
    //Rotate_x(angulo);
 	angulo = startAngulo * PI/180;
@@ -187,9 +193,10 @@ void render(){
 
    //double new_x, new_y, new_z;
 
-   for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+   //for(auto i = 0; i < Size; i++){
+      //for(auto j = 0; j < Size; j++){
+	  for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
 
             new_x = Cube[i + Size * (j + Size * k)].x * matrix_y[0][0] + Cube[i + Size * (j + Size * k)].y * matrix_y[0][1] + Cube[i + Size * (j + Size * k)].z * matrix_y[0][2];
             new_y = Cube[i + Size * (j + Size * k)].x * matrix_y[1][0] + Cube[i + Size * (j + Size * k)].y * matrix_y[1][1] + Cube[i + Size * (j + Size * k)].z * matrix_y[1][2];
@@ -199,53 +206,65 @@ void render(){
             Cube[i + Size * (j + Size * k)].y = new_y;
             Cube[i + Size * (j + Size * k)].z = new_z;
 
-         }
+         //}
       }
-   }
+   //}
    //OffSet_Z(OffSet);
    offset = OffSet;
-  for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+  //for(auto i = 0; i < Size; i++){
+      //for(auto j = 0; j < Size; j++){
+	  for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
             Cube[i + Size * (j + Size * k)].z += offset;
-         }
+         //}
       }
-   }
+   //}
    //Transform_3D_2D();
    
    double dist_to_screen = 300;
    //double new_x, new_y;
 
-   for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+   //for(auto i = 0; i < Size; i++){
+      //for(auto j = 0; j < Size; j++){
+	  for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
             new_x = Cube[i + Size * (j + Size * k)].x*dist_to_screen/Cube[i + Size * (j + Size * k)].z+width/2;
             new_y = Cube[i + Size * (j + Size * k)].y*dist_to_screen/Cube[i + Size * (j + Size * k)].z+width/2;
             //Cube_Perspective[i][j][k].set_pos(new_x, new_y, 0, Cube[i + Size * (j + Size * k)].cor);
             Cube_Perspective[i + Size * (j + Size * k)].set_pos(new_x, new_y, 0, Cube[i + Size * (j + Size * k)].cor);
-         }
+         //}
       }
-   }
+   //}
    //OffSet_Z(-OffSet);
    offset = -OffSet;
-   for(auto i = 0; i < Size; i++){
-      for(auto j = 0; j < Size; j++){
-         for(auto k = 0; k < Size; k++){
+   //for(auto i = 0; i < Size; i++){
+      //for(auto j = 0; j < Size; j++){
+	  for(auto j = (threadIdx.x*10)%100; j < ((threadIdx.x*10)%100)+10; j++){
+         //for(auto k = 0; k < Size; k++){
             Cube[i + Size * (j + Size * k)].z += offset;
-         }
+         //}
       }
-   }
+   //}
 }
 
 
 int main(void)
 {
 
-   Size = 100;
-   width = 2000;
-   OffSet = 3000;
-   int frames = 1;
-   double spacing_factor = 40;
+	cudaDeviceProp devProp;
+	cudaGetDeviceProperties(&devProp, 0);
+	int maxThreadsPerBlock = devProp.maxThreadsPerBlock;
+	int maxBlocks = devProp.maxGridSize[0];
+	std::cout << "max threads per block: " << maxThreadsPerBlock << std::endl << "max blocks: " << maxBlocks << std::endl;
+
+	Cube_Unity *Cube;
+	Cube_Unity *Cube_Perspective;
+	
+	int Size = 100;
+	int width = 2000;
+	int OffSet = 3000;
+	int frames = 1;
+	double spacing_factor = 40;
 
    double inix, iniy, iniz;
    double atualx, atualy, atualz;
@@ -255,8 +274,11 @@ int main(void)
    spacing = -inix*2 / double(Size);
 
    //Cube = new Cube_Unity**[Size];
-   Cube = new Cube_Unity[Size*Size*Size];
-   Cube_Perspective = new Cube_Unity[Size*Size*Size];
+   
+   //Cube = new Cube_Unity[Size*Size*Size];
+   cudaMallocManaged(&Cube, Size*Size*Size*sizeof(Cube_Unity));
+   //Cube_Perspective = new Cube_Unity[Size*Size*Size];
+   cudaMallocManaged(&Cube_Perspective, Size*Size*Size*sizeof(Cube_Unity));
    //Cube_Perspective = new Cube_Unity**[Size];
    for(auto i = 0; i < Size; i++){
        //Cube[i] = new Cube_Unity*[Size];
@@ -287,9 +309,15 @@ int main(void)
       }
    }
 
+	int max_threads, max_blocks;
 
+	max_threads = 1000;
+	max_blocks = Size;
+	
    for(int frame = 0; frame < frames; frame++){
-      render();
+	  render<<<max_blocks, max_threads>>>(Cube, Cube_Perspective);
+      //render();
+	  cudaDeviceSynchronize();
       for(auto i = 0; i < Size; i++){
          for(auto j = 0; j < Size; j++){
             for(auto k = 0; k < Size; k++){
